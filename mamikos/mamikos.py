79c7bb747e84deff5a7b20983d88e975551a0865
Marco TB <alvinmarco8@gmail.com>
2024-02-09 14:04:32 +0700

    update

--- mamikos/mamikos.py
+++ mamikos/mamikos.py
@@ -11,8 +11,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait, Select
 
-executable_path = r"C:\Users\817932702\mamikos\chromedriver\120\chromedriver.exe"
-# executable_path = r"D:\proyek\chromedriver\120\chromedriver.exe"
+# executable_path = r"C:\Users\817932702\mamikos\chromedriver\120\chromedriver.exe"
+executable_path = r"D:\proyek\chromedriver\120\chromedriver.exe"
+# executable_path = r'D:\proyek\scrapper\chromedriver\120\chromedriver.exe'
 service = Service(executable_path)
 
 options = webdriver.ChromeOptions()
@@ -51,9 +52,6 @@ kota_2.click()
 
 time.sleep(5)
 
-# oke_button = driver.find_element(By.XPATH, '//*[@id="desktopBannerWrapped"]/div/div[3]/div[1]/button[1]')
-# oke_button.click()
-
 list_df = []
 
 # Inisialisasi indeks awal
@@ -61,67 +59,21 @@ start_index = 1
 
 while True:
     try:
-        for i in range(start_index, start_index + 20):
-            xpath = '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[' + str(i) + ']'
-            
-            
-            try:
-                elemen = driver.find_element(By.XPATH, xpath)
-                isi_elemen = elemen.text
-                data_elemen = isi_elemen.split('\n')
+        # Coba temukan next_button
+        next_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[2]/div/a')
+        next_button.click()
 
-                df = pd.DataFrame({
-                    'Jenis': [data_elemen[0]],
-                    'Status Kamar': [data_elemen[1]] if len(data_elemen) > 1 else None,
-                    'Nama Kost': [data_elemen[2]] if len(data_elemen) > 1 else None,
-                    'Lokasi': [data_elemen[3]],
-                    'Fasilitas': [data_elemen[4]],
-                    'Rating': [data_elemen[5]],
-                    'Diskon': [data_elemen[6]] if len(data_elemen) > 1 else None,
-                    'Harga': [data_elemen[7]]
-                })
+        # Tunggu hingga halaman selanjutnya dimuat
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[1]'))
+        )
 
-                list_df.append(df)
-
-                        # Coba temukan next_button
-                next_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[2]/div/a')
-                next_button.click()
-
-                # Tunggu hingga halaman selanjutnya dimuat
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[1]'))
-                )
-
-                # Scroll ke bawah untuk memuat lebih banyak data
-                scroll_down()
-                time.sleep(2)  # Tunggu sebentar setelah setiap scroll
-
-            except UnexpectedAlertPresentException:
-                # Tangani popup dengan menekan tombol OK
-                alert = driver.switch_to.alert
-                alert.accept()
-                print("Popup ditangani.")
-            except ElementNotInteractableException:
-                # Tangani popup dengan menekan tombol OK
-                alert_button_xpath = '//*[@id="filterKostTypeWrapper"]/div/div[1]/div[1]/div/div/div[3]/button'
-                try:
-                    alert_button = driver.find_element(By.XPATH, alert_button_xpath)
-                    alert_button.click()
-                    print("Popup ditangani.")
-                except NoSuchElementException:
-                    print("Tidak dapat menemukan tombol OK pada popup.")
-                    break
-
-        # Gabungkan semua DataFrames menjadi satu
-        final_df = pd.concat(list_df, ignore_index=True)
+        scroll_down()
+        time.sleep(2)
 
         # Atur ulang indeks
         start_index += 20
-        print(final_df)
 
-    # except NoSuchElementException:
-    #     # Jika next_button tidak ditemukan, keluar dari loop
-    #     break
     except ElementNotInteractableException:
         # Tangani popup dengan menekan tombol OK
         alert_button_xpath = '//*[@id="filterKostTypeWrapper"]/div/div[1]/div[1]/div/div/div[3]/button'
@@ -135,4 +87,26 @@ while True:
     except NoSuchElementException:
         break
 
+time.sleep(3)
 
+nama_kos = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div/div/div[2]/div[2]/div/span[1]')
+alamat_kos = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div/div/div[2]/div[2]/div/span[2]')
+fasilitas = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div/div/div[2]/div[3]/div')
+harga = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div/div/div[2]/div[4]/div/div/div/span[1]')
+
+# Buat list untuk menyimpan data
+data = []
+
+for nama, alamat, fasilitas, harga in zip(nama_kos, alamat_kos, fasilitas, harga):
+    data.append({
+        'Nama Kos': nama.text,
+        'Alamat Kos': alamat.text,
+        'Fasilitas': fasilitas.text,
+        'Harga': harga.text
+    })
+    print("Berhasil")
+
+    # Buat DataFrame dari list data
+    df = pd.DataFrame(data)
+
+df.to_csv('output.csv', header=False)

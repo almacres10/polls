421a6499df834ec6f609e8296d0389e81e48e728
ALVIN MARCO CRISTY TAMPUBOLON <817932702@intranet.pajak.go.id>
2024-02-15 16:16:37 +0700

    update

--- mamikos/mamikos.py
+++ mamikos/mamikos.py
@@ -1,21 +1,13 @@
-import pandas as pd
-from datetime import datetime
-import os, sys, re
-import time
+import re, pandas as pd, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, JavascriptException, UnexpectedAlertPresentException, ElementNotInteractableException
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support.ui import WebDriverWait
 
-# executable_path = r"C:\Users\817932702\mamikos\chromedriver\120\chromedriver.exe"
-executable_path = r"D:\proyek\chromedriver\120\chromedriver.exe"
-# executable_path = r'D:\proyek\scrapper\chromedriver\120\chromedriver.exe'
+executable_path = r"C:\Users\817932702\mamikos\chromedriver\120\chromedriver.exe"
 service = Service(executable_path)
-
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -30,28 +22,15 @@ def scroll_down():
 start_time = time.time()
 
 driver.get("https://mamikos.com/") 
-
-time.sleep(5)
-
-cari = driver.find_element(By. XPATH, '//*[@id="home"]/div[4]/div/div/div/div[1]')
+cari = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div[4]/div/div/div/div[1]')))
 cari.click()
-
-time.sleep(2)
-
-area = driver.find_element(By. XPATH, '//*[@id="home"]/div[13]/div/div[2]/div[2]/ul/li[2]/a')
+area = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div[13]/div/div[2]/div[2]/ul/li[2]/a')))
 area.click()
-
-time.sleep(2)
-
-kota = driver.find_element(By. XPATH, '//*[@id="home"]/div[13]/div/div[2]/div[4]/div/div[25]/details/summary')
+kota = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div[13]/div/div[2]/div[4]/div/div[25]/details/summary')))
 kota.click()
-
-time.sleep(2)
-
-kota_2 = driver.find_element(By. XPATH, '//*[@id="home"]/div[13]/div/div[2]/div[4]/div/div[25]/details/div/a[5]')
+kota_2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="home"]/div[13]/div/div[2]/div[4]/div/div[25]/details/div/a[5]')))
 kota_2.click()
 
-time.sleep(5)
 
 list_df = []
 
@@ -86,34 +65,24 @@ while True:
             print("Tidak dapat menemukan tombol OK pada popup.")
             break
     except NoSuchElementException:
+        print("Tidak ada next")
         break
 
-time.sleep(3)
+elemen_nama = driver.find_elements(By. CLASS_NAME, 'rc-info')
+elemen_fasilitas = driver.find_elements(By. CLASS_NAME, 'rc-facilities')
+elemen_harga = driver.find_elements(By. CLASS_NAME, 'rc-price__text')
 
-
-nama_kos = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div/div/div[2]/div[2]/div/span[1]')
-alamat_kos = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div/div/div[2]/div[2]/div/span[2]')
-fasilitas = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div[2]/div/div[2]/div[3]/div[not(@class="rc-facilities__rating")]')
-harga = driver.find_elements(By.XPATH, '//*[@id="app"]/div/div[5]/div/div[1]/div/div/div[1]/div[1]/div[*]/div/div[2]/div/div[2]/div[4]/div/div[2]/div/span[1]')
-
-# Buat list untuk menyimpan data
 data = []
 
-for nama, alamat, fasilitas, harga in zip(nama_kos, alamat_kos, fasilitas, harga):
+for nama, fasilitas, harga in zip(elemen_nama, elemen_fasilitas, elemen_harga):
     fasilitas_text = re.sub(r'[^a-zA-Z0-9\s]', '', fasilitas.text)
 
+
     data.append({
-        'Nama Kos': nama.text,
-        'Alamat Kos': alamat.text,
+        'Nama_Kos': nama.text,
         'Fasilitas': fasilitas_text,
         'Harga': harga.text
     })
 
 # Membuat DataFrame dari list data setelah loop selesai
 df = pd.DataFrame(data)
-
-df.to_csv('output3.csv', index=False)
-
-end_time = time.time()
-execution_time = end_time - start_time
-print("Waktu eksekusi:", execution_time, "detik")
